19518c1ab0ac43f502a9a284b479a6692f4e3d0f
jaehafe <wogkdkrm112@gmail.com>
2022-07-15 23:35:13 +0900

    탭기능 js 구현
    +for문

--- detail.js
+++ detail.js
@@ -3,31 +3,31 @@
 // -버튼 0에 orange 클래스명 추가
 // -모든div에 붙은 show 클래스명 제거
 // -div0에 show 클래스명 추가
-
+//
 for (let i = 0; i < 3; i++){
 
-// var 버튼 = $('.tab-button');
-
-$('.tab-button').eq(i).on('click', function(){
-  $('.tab-button').removeClass('orange');
-  $('.tab-button').eq(i).addClass('orange');
-  $('.tab-content').removeClass('show');
-  $('.tab-content').eq(i).addClass('show');
-});
-
-}
-
-// $('.tab-button').eq(1).on('click', function(){
-//   $('.tab-button').removeClass('orange');
-//   $('.tab-button').eq(1).addClass('orange');
-//   $('.tab-content').removeClass('show');
-//   $('.tab-content').eq(1).addClass('show');
-// });
-
-// $('.tab-button').eq(2).on('click', function(){
-//   $('.tab-button').removeClass('orange');
-//   $('.tab-button').eq(2).addClass('orange');
-//   $('.tab-content').removeClass('show');
-//   $('.tab-content').eq(2).addClass('show');
-// });
+  // var 버튼 = $('.tab-button');
+  
+  $('.tab-button').eq(i).on('click', function(){
+    $('.tab-button').removeClass('orange');
+    $('.tab-button').eq(i).addClass('orange');
+    $('.tab-content').removeClass('show');
+    $('.tab-content').eq(i).addClass('show');
+  });
+  
+  }
+  
+  // $('.tab-button').eq(1).on('click', function(){
+  //   $('.tab-button').removeClass('orange');
+  //   $('.tab-button').eq(1).addClass('orange');
+  //   $('.tab-content').removeClass('show');
+  //   $('.tab-content').eq(1).addClass('show');
+  // });
+  
+  // $('.tab-button').eq(2).on('click', function(){
+  //   $('.tab-button').removeClass('orange');
+  //   $('.tab-button').eq(2).addClass('orange');
+  //   $('.tab-content').removeClass('show');
+  //   $('.tab-content').eq(2).addClass('show');
+  // });
 
